d34629a87af4fe9f0d6047a64f464b9a76bce337
Sunatillo2024 <171573809+Sunatillo2024@users.noreply.github.com>
2025-09-26 21:22:29 +0500

    fixed start_hl.py file

--- handlers/start_hl.py
+++ handlers/start_hl.py
@@ -8,18 +8,22 @@ router = Router()
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
+    gif_url = "https://media.giphy.com/media/xT9IgG50Fb7Mi0prBC/giphy.gif"
+    caption = (
+        "👋 Assalomu alaykum!\n"
+        "🏋️‍♂️ Fitness Planner Bot ga xush kelibsiz!\n"
+        "✨ Bu yerda siz mashg‘ulotlaringizni rejalashtirasiz va natijalaringizni kuzatib borasiz."
+    )
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    await message.answer_animation(
+        animation=gif_url,  # 🔑 MUHIM — bu argument berilishi kerak
+        caption=caption
+    )
+
 
 @router.message()
 async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
     try:
-        # Send a copy of the received message
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
